31ffd63aa40b9de46d23db828396c86d227afad1
Ajay <ajaynsrinivas@gmail.com>
2018-02-28 14:20:46 +0530

    initial version

---
+++ db/mongoose.js
@@ -0,0 +1,8 @@
+//CustomerInfo
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connect('mongodb://localhost:27017/CustomerInfo');
+
+module.exports = db;
